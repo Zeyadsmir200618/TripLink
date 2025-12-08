2cc07fe48d82ecbfe6b0d2570222d2f6b634b18d
Zeyad mohamed <Zeyadsamir27@gmail.com>
2025-12-08 23:42:52 +0200

    Update and rename connection.php to database.php

---
+++ Config/database.php
@@ -0,0 +1,18 @@
+<?php
+class Database {
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "booking_app_db";
+    public $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        if ($this->conn->connect_error) {
+            die("❌ Database connection failed: " . $this->conn->connect_error);
+        }
+        $this->conn->set_charset("utf8");
+    }
+}
+?>
+
